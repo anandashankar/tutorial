c05383dd68e459da92e2c163048de2015ffd2a61
anandashankar <anandaschakraborti@gmail.com>
2021-05-20 23:39:43 +0300

    added for logic on cars array

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
+var cars = ["BMW", "Audi", "Volvo", "Mercedes"];
+var text = "";
+var i;
+
+for(i=0;i<=cars.length;i++){
+  text += cars[i] + "<br>";
+}
+
 console.log("Hello git from test");
+
+
+
